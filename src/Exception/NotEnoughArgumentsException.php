db256fee840257433230887e5184836bd5b098a2
khalyomede <>
2018-11-04 21:29:52 +0100

    added exception when not enough arguments are passed to a command

--- src/Exception/NotEnoughArgumentsException.php
+++ src/Exception/NotEnoughArgumentsException.php
@@ -1,7 +1,30 @@
 <?php
     namespace Khalyomede\Exception;
 
-    use RuntimeException;
+    use RuntimeException;    
 
-    class NotEnoughArgumentsException extends RuntimeException {}
+    class NotEnoughArgumentsException extends RuntimeException {
+        protected $userNumberOfArgument;
+        protected $requiredNumberOfArgument;
+
+        public function setUserNumberOfArgument(int $count): self {
+            $this->userNumberOfArgument = $count;
+
+            return $this;
+        }
+
+        public function setRequiredNumberOfArgument(int $count): self {
+            $this->requiredNumberOfArgument = $count;
+
+            return $this;
+        }
+
+        public function getUserNumberOfArgument(): int {
+            return $this->userNumberOfArgument;
+        }
+
+        public function getRequiredNumberOfArgument(): int {
+            return $this->requiredNumberOfArgument;
+        }
+    }
 ?>
